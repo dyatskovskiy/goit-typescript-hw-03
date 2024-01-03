f6082f9880944c51fcf2b6d11cba48cce48cbd68
dyatskovskiy <d.yatskovskiy@ukr.net>
2024-01-03 20:10:46 +0200

    fixed 2nd task

--- src/2.ts
+++ src/2.ts
@@ -33,8 +33,8 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor(name: string, department: string) {
-    super(name, department, 10000)
+  constructor(name: string, department: string, salary: number) {
+super(name, department, salary + 10000)
   }
 }
 
